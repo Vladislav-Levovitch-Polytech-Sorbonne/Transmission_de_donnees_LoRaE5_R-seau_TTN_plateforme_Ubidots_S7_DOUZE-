cf8af174ec5e10ae1fb488cc6aba144cdfd94d70
Vladislav-Levovitch-Polytech-Sorbonne <vladislav.balayan@etu.sorbonne-universite.fr>
2024-10-08 13:12:37 +0200

    Transmission Donnees ESP to LoRa to NNT - 1 avec LOG d affichage

---
+++ scr/Transmission_AT_ESP_to_LoRa/Transmission_AT_ESP_to_LoRa.ino
@@ -0,0 +1,67 @@
+//#include <LoRa-E5.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial Serial_to_LoRa (D5, D6); // D5 : GPIO14 Rx to Lora D6 : GPIO12 Tx to Lora 
+// GPIO3 to RX, GPIO1 to TX reserved to ESP8266 to USB convert
+
+void setup()
+{
+  delay (3000);
+  Serial.begin(9600); //Monitor
+  Serial_to_LoRa.begin(9600); //LoRa  
+  delay (2000);
+
+  Serial_to_LoRa.write ("AT+ID=DevEUI,70B3D57ED006AE59 ,");
+  Serial.println("\n\nSerial_to_LoRa \"AT+ID=DevEUI\"");
+  delay (1000);
+  Serial_to_LoRa.write ("AT+ID=AppEUI,A0A0A0A0A0A0A0A0 ");
+  Serial.println("Serial_to_LoRa \"AT+ID=AppEUI\"");
+  delay (1000);
+  Serial_to_LoRa.write ("AT+KEY=APPKEY,B3C21FF5B9ECD87A0BB702B2C3D87459 ");
+  Serial.println("Serial_to_LoRa \"AT+KEY=APPKEY\"\n");
+  delay (1000);
+
+  Serial_to_LoRa.write ("AT+DR=EU868");
+  Serial.println("Serial_to_LoRa \"AT+DR=EU868\"");
+  delay (2000);
+
+  Serial_to_LoRa.write ("AT+MODE=LWOTAA ");
+  Serial.println("Serial_to_LoRa \"AT+MODE=LWOTAA \"");
+  delay (2000);
+
+  Serial_to_LoRa.write ("AT+JOIN");
+  Serial.println("Serial_to_LoRa \"AT+JOIN\"");
+  delay (2000);
+  Serial_to_LoRa.write ("AT");
+  Serial.println("Serial_to_LoRa \"AT\"");
+  
+  
+
+}
+
+void loop() 
+{
+  if (Serial_to_LoRa.available ())
+  {
+    delay (1000);
+    Serial_to_LoRa.write ("AT+MSG=1");
+    Serial.println("Serial_to_LoRa \"AT+MSG=1\"");
+    delay (4000);
+  }
+
+  else 
+  {
+    Serial.print ("Serial_to_LoRa not available");
+    delay (500);
+    // Pause visuelle
+    for (int i = 0; i < 8; i++) 
+    {
+      Serial.printf(" .");
+      if (i == 7) 
+      {
+        Serial.print("\n");
+      }
+      delay(200);
+    } 
+  }
+}
