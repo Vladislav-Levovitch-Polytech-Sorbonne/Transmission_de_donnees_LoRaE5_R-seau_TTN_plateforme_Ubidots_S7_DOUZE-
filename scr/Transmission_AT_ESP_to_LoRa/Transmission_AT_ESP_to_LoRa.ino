930c41d7382a6b2eaac2a51d1427065027eaaed0
Vladislav-Levovitch-Polytech-Sorbonne <vladislav.balayan@etu.sorbonne-universite.fr>
2024-10-08 13:24:42 +0200

    Transmission Donnees ESP to LoRa to NNT - 2 msg dynamique

--- scr/Transmission_AT_ESP_to_LoRa/Transmission_AT_ESP_to_LoRa.ino
+++ scr/Transmission_AT_ESP_to_LoRa/Transmission_AT_ESP_to_LoRa.ino
@@ -4,6 +4,8 @@
 SoftwareSerial Serial_to_LoRa (D5, D6); // D5 : GPIO14 Rx to Lora D6 : GPIO12 Tx to Lora 
 // GPIO3 to RX, GPIO1 to TX reserved to ESP8266 to USB convert
 
+//Creer mini fonction pour l affichage synthetique type Serial_to_LoRa_function (str commande, str texte_affiche, int delay )
+
 void setup()
 {
   delay (3000);
@@ -41,27 +43,35 @@ void setup()
 
 void loop() 
 {
-  if (Serial_to_LoRa.available ())
-  {
-    delay (1000);
-    Serial_to_LoRa.write ("AT+MSG=1");
-    Serial.println("Serial_to_LoRa \"AT+MSG=1\"");
-    delay (4000);
-  }
-
-  else 
-  {
-    Serial.print ("Serial_to_LoRa not available");
-    delay (500);
-    // Pause visuelle
-    for (int i = 0; i < 8; i++) 
+  for (int ii = 1; ii < 10; ii++)
+{
+    if (Serial_to_LoRa.available())
     {
-      Serial.printf(" .");
-      if (i == 7) 
-      {
-        Serial.print("\n");
-      }
-      delay(200);
-    } 
-  }
+        delay(1000);
+
+        // Utilisation d'un seul print pour AT+MSG avec la valeur de ii
+        Serial_to_LoRa.print("AT+MSG=");
+        Serial_to_LoRa.println(ii); // Incrémente le numéro
+        Serial.println("Serial_to_LoRa \"AT+MSG=" + String(ii) + "\"");
+
+        delay(4000);
+    }
+    else
+    {
+        Serial.print("Serial_to_LoRa not available");
+        delay(500);
+
+        // Pause visuelle
+        for (int i = 0; i < 8; i++)
+        {
+            Serial.printf(" .");
+            if (i == 7)
+            {
+                Serial.print("\n");
+            }
+            delay(200);
+        }
+    }
+}
+
 }
